574cf63340cc72f3f1006fba7b60127e3004f7a7
jobinnevin <jobin.nevin@gmail.com>
2023-12-09 21:18:26 -0500

    assignment2

---
+++ Assignment2/ejs/index.js
@@ -0,0 +1,5 @@
+import { calculateArea, calculateVolume, calculateSquare } from './calculate.js';
+
+console.log(calculateArea(4, 2)); // 8
+console.log(calculateVolume(2, 2, 2)); // 8
+console.log(calculateSquare(4)); // 16
