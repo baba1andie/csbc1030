7c8e9915755d45f4507b7812f8539771896f85b7
ShreeLakshmiA <shreelakshmi.athinarayanan@gmail.com>
2023-12-15 14:43:45 -0500

    Assignment05 submission

---
+++ middleware/middlewares.js
@@ -0,0 +1,9 @@
+function someMiddleware(req, res, next) {
+  console.time("Request");
+  console.log(`METHOD: ${req.method}; URL: ${req.url}`);
+
+  next();
+  console.timeEnd("Request");
+}
+
+module.exports = someMiddleware;
