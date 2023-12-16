0307647045a11013918e1aedb599ceca7bac1d11
hp <hp@DESKTOP-LDKE30N>
2023-12-15 22:46:01 -0500

    Assignment 5

---
+++ assignment4/routes/users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { appendUser } = require('../controllers/users');
+
+router.post('/users', appendUser);
+
+module.exports = router;
