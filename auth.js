33dadf71f4e8733b0b5b26ba9cb9db96cdc73e5a
Madhumithacodes <144867428+Madhumithacodes@users.noreply.github.com>
2023-12-16 21:26:08 -0500

    Assignment6

---
+++ auth.js
@@ -0,0 +1,37 @@
+// auth.js
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+// const { secretKey } =process.env.SECRET_KEY;//require('./config'); 
+// console.log('Secret Key:', process.env.SECRET_KEY);
+function auth(req, res, next) {
+  const token = req.headers.authorization;
+  console.log(token);
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized!!' });
+  }
+
+  try {
+    console.log("try");
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    console.log(decoded);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error.message);
+    return res.status(401).json({ error: 'Unauthorized - Invalid token'});
+  }
+}
+
+module.exports = auth;
+function authorize(userID) {
+    return (req, res, next) => {
+      if (req.user.id === userID) {
+        next();
+      } else {
+        return res.status(403).json({ error: 'You are not authorized!!!' });
+      }
+    };
+  }
+  
+  module.exports = { auth, authorize };
+
