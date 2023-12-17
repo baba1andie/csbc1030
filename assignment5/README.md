53005dc3cbcf6fdcfb88784904aeddf734c9ebf3
PoojaGadge3 <poojagadge3498@gmail.com>
2023-12-17 05:59:58 -0500

    assignment 7

--- assignment5/README.md
+++ assignment5/README.md
@@ -1,43 +1,62 @@
-# To run this assignment 6
+# To run this assignment 7
 # In terminal
 
 1. To get path of assignment 
-   cd assignment5
+cd assignment5
 
 2. install necessary packages 
 npm init     
 npm install express
 npm install jsonwebtoken
+npm npm install sequelize mysql2
 
-3. to run the code 
+# MySQL Server
+3. Craete a database called 'assignment_7' in MySQL 
+CREATE DATABASE assignment_7; 
+INSERT INTO user (username, password, first_name, last_name) VALUES ('abcTest', 'abc@123', 'abc', 'test');
+
+# For database connection : update db.js file in model folder
+4. Replace 'database_name', 'username', 'password' with your MySQL database name, username and password.
+5. Modify the 'host' according to your MySQL server configuration.
+
+# To run the code : In Terminal
+6. to run the code 
  node index.js
 
-# For API in Postman
+# To run API in Postman
 
-1. To get login
+7. GET `/users` that returns all users
+http://localhost:3000/users
+
+8. POST `/users/login` that returns an authentication token to be used in future requests.
 http://localhost:3000/users/login
 {
-  "username": "pooja_G",
-  "password": "password123"
+  "username": "abcTest",
+  "password": "abc@123"
 }
 
-2. GET `/users/:id` that returns a user with a specific id
+9. GET `/users/:id` that returns a user with  a specific id if it matches the sender of the request
    http://localhost:3000/users/1
 
-   in headers add
-   Authorization : Bearer [token]
+   In Headers add 
+   key : Authorization
+   Value : Bearer [token]
+   
+   Replace token with your token which generated in login api as a return.
 
 3. POST `/users` that add a new user 
 http://localhost:3000/users
   
-  in headers add
-   Authorization : Bearer [token]
+  In Headers add 
+   key : Authorization
+   Value : Bearer [token]
+   
+   Replace token with your token which generated in login api as a return.
 
-    add below json code as an input in body 
+   Add below json code as an input in body 
    {
-    "id": 4,
-    "username": "new_user",
-    "password": "itsAbc",
-    "first_name": "aaa",
-    "last_name": "bbb"
+    "username": "xyzTest",
+    "password": "xyz@123",
+    "first_name": "xyz",
+    "last_name": "test"
 }
--- assignment5/routes/userRoutes.js
+++ assignment5/routes/userRoutes.js
@@ -2,16 +2,14 @@
 
 const express = require("express");
 const router = express.Router();
-const usersData = require("../samples/users.json");
-const fs = require('fs');
 const authenticateToken = require('../middleware/authmiddleware');
 const jwt = require('jsonwebtoken');
+const User = require('../model/userModel');
 
-router.post('/login', (req, res) => {
+router.post('/login', async(req, res) => {
   const { username, password } = req.body;
 
-  const user = usersData.find(u => u.username === username && u.password === password);
-
+  const user = await User.findOne({ where: { username, password } });
   if (!user) {
     return res.status(401).json({ error: 'Unauthorized: Invalid credentials' });
   }
@@ -20,46 +18,50 @@ router.post('/login', (req, res) => {
   res.status(200).json({ token });
 });
 
-router.get("/", (req, res) => {
-  res.json(usersData);
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
-router.get('/:id', authenticateToken, (req, res) => {
-  const requestedUserId = req.params.id;
-  const authenticatedUserId = req.user.userId;
-
-  if (requestedUserId == authenticatedUserId) {
-    const user = usersData.find(u => u.id == requestedUserId);
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
+router.get('/:id', authenticateToken, async (req, res) => {
+  try {
+    const requestedUserId = req.params.id;
+    const authenticatedUserId = req.user.userId;
   
-    res.status(200).json(user);
-   
+    if (requestedUserId == authenticatedUserId) {
+      const user = await User.findByPk(requestedUserId);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+    
+      res.status(200).json(user);
+    }
+    else {
+      return res.status(403).json({ error: 'Forbidden: Access denied' });
+    }
+  } catch(error) {
+    res.status(500).json({ error: 'Server error' });
   }
-  else {
-
-    return res.status(403).json({ error: 'Forbidden: Access denied' });
-  }
+ 
 
 });
 
-router.post("/", authenticateToken, (req, res) => {
-  const authenticatedUserId = req.user.userId;
-  if (authenticatedUserId !== 1) {
-    return res.status(403).json({ error: 'Forbidden: Access denied' });
-  }
-  
-  const newUser = req.body;
-  usersData.push(newUser);
-
-  fs.appendFile('users.json', JSON.stringify(usersData), (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Failed to add user' });
+router.post("/", authenticateToken, async (req, res) => {
+  try {
+    const authenticatedUserId = req.user.userId;
+    if (authenticatedUserId !== 1) {
+      return res.status(403).json({ error: 'Forbidden: Access denied' });
     }
-    res.status(201).json({ message: 'User added successfully', user: newUser });
-  });
+  
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 module.exports = router;
