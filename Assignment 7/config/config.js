118dc1139d7b972afb27efe9d8a56ddc3cd0236b
Aditya Kuchhal <adikuchhal@gmail.com>
2023-12-18 00:34:29 -0500

    Aditya Kuchhal - Assignment 7

---
+++ Assignment 7/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "adi@mysql7",
+    database: "assignment7_development",
+    host: "localhost",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: "adi@mysql7",
+    database: "assignment7_production",
+    host: "localhost",
+    dialect: "mysql",
+  },
+};
