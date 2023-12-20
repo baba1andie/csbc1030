0101c66e6a1c1597032d827f8aad9ec0bef94ae0
Sahej1204 <makkarsahej311@gmail.com>
2023-12-20 18:35:47 -0500

    Assignment008

---
+++ Assignment008/models/db.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('csbc1030', 'root', 'Madhu@2610', { //modify your creds 
+  dialect: 'mysql',
+  host: 'localhost',
+});
+
+module.exports = 
+  sequelize;
