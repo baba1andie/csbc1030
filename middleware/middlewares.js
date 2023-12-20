9623e3d8fa650460daf366db084828b6cb799680
ShreeLakshmiA <shreelakshmi.athinarayanan@gmail.com>
2023-12-20 09:35:19 -0500

    adding bad request

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -1,8 +1,9 @@
 const { findTokenInCookie } = require("../services/authService.js");
 
-const someMiddleware = async (req, res, next) => {
+const authFilter = async (req, res, next) => {
   console.time("Request");
   console.log(`METHOD: ${req.method}; URL: ${req.url}}`);
+
   try {
     await findTokenInCookie(req);
     next();
@@ -13,4 +14,8 @@ const someMiddleware = async (req, res, next) => {
   console.timeEnd("Request");
 };
 
-module.exports = someMiddleware;
+const badRequest = async (req, res) => {
+  return res.status(500).send("Bad Request");
+};
+
+module.exports = { authFilter, badRequest };
